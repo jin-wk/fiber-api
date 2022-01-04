45b2d364f84f24f8d1adaf7afb265252c48cf8af
Teddy <note@kakao.com>
2022-01-04 12:16:19 +0900

    [220104] Model fix

--- models/auth.go
+++ models/auth.go
@@ -8,13 +8,11 @@ import (
 )
 
 type RegisterUser struct {
-	ID              int       `json:"id" swaggerignore:"true"`
-	Email           string    `json:"email" validate:"required,email,min=10,max=50" example:"example@example.com"`
-	Password        string    `json:"password" validate:"required_with=password_confirm,eqfield=PasswordConfirm,min=6,max=20" example:"example"`
-	PasswordConfirm string    `json:"password_confirm" example:"example"`
-	Name            string    `json:"name" validate:"required,min=2,max=20" example:"example"`
-	CreatedAt       time.Time `json:"created_at" swaggerignore:"true"`
-	UpdatedAt       time.Time `json:"updated_at" swaggerignore:"true"`
+	ID              int    `json:"id" swaggerignore:"true"`
+	Email           string `json:"email" validate:"required,email,min=10,max=50" example:"example@example.com"`
+	Password        string `json:"password" validate:"required_with=password_confirm,eqfield=PasswordConfirm,min=6,max=20" example:"example"`
+	PasswordConfirm string `json:"password_confirm" example:"example"`
+	Name            string `json:"name" validate:"required,min=2,max=20" example:"example"`
 }
 
 func (u *RegisterUser) BeforeSave(tx *gorm.DB) error {
@@ -24,6 +22,10 @@ func (u *RegisterUser) BeforeSave(tx *gorm.DB) error {
 	}
 	u.Password = string(hash)
 	return nil
+}
+
+func (RegisterUser) TableName() string {
+	return "users"
 }
 
 type User struct {
