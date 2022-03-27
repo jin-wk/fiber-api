4de2bf0c4fb71ee1f673fe33c0e4208f2cf8460b
Teddy <note@kakao.com>
2022-03-28 01:52:05 +0900

    [220328] fix

--- utils/response.go
+++ utils/response.go
@@ -2,11 +2,9 @@ package utils
 
 import "github.com/gofiber/fiber/v2"
 
-type response struct {
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
-
 func Response(c *fiber.Ctx, status int, message string, data interface{}) error {
-	return c.Status(status).JSON(response{message, data})
+	return c.Status(status).JSON(fiber.Map{
+		"message": message,
+		"data":    data,
+	})
 }
